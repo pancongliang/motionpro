bb6b915df1994ecb10d46a8370fa151ce0814c42
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-02 01:15:57 +0800

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -3,7 +3,7 @@ RUNIT='MotionPro_Linux_RedHat_x64_build-8383-30.sh'
 if test -f "$RUNIT"; then
    echo "$RUNIT exists."
 else
-   wget --no-check-certificate -N https://support.arraynetworks.net/prx/000/http/supportportal.arraynetworks.net/downloads/pkg_9_4_0_505/MP_Linux_1.2.18/$RUNIT
+   wget --no-check-certificate -N https://support.arraynetworks.net/prx/000/http/supportportal.arraynetworks.net/downloads/pkg_9_4_5_8/MP_Linux_1.2.18/$RUNIT
 fi
 
 podman build -t motion-pro-vpn-client .
