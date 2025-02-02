8cd16f8d8ffd924e7c2bdf1f7fb616633a923cde
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-02 22:35:33 +0800

    Create runit-host-network.sh

---
+++ runit-host-network.sh
@@ -0,0 +1,1 @@
+podman run -d --hostname motion-pro-vpn --name VPNcontainer --privileged --network host -v /root:/host-rootfs motion-pro-vpn-client --method $METHOD --host $HOST --user $USER --passwd $PASSWD -c inf --loglevel warn
