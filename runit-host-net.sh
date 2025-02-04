5ce7c143d692d99a3bd4acc15f1346bb7ec0851d
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-02-04 13:06:09 +0800

    Update runit-host-net.sh

--- runit-host-net.sh
+++ runit-host-net.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 podman run -d --hostname motion-pro-vpn --name VPNcontainer --privileged --network host -v /root:/host-rootfs motion-pro-vpn-client --method $METHOD --host $HOST --user $USER --passwd $PASSWD -c inf --loglevel warn
