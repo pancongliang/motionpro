b54bba92eca381aa4f921b3fadb60aeb88f1ef68
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-11-07 19:03:29 +0800

    Delete runit-host-net.sh

--- runit-host-net.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-podman run -d --hostname motion-pro-vpn --name VPNcontainer --privileged --network host -v /root:/host-rootfs motion-pro-vpn-client --method $METHOD --host $HOST --user $USER --passwd $PASSWD -c inf --loglevel warn
