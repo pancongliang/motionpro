0b672618c4691ffc4d7e9d457cf689cd09e3b75a
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-11-07 19:03:37 +0800

    Delete runit.sh

--- runit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-podman run -d \
-  --hostname motion-pro-vpn --name VPNcontainer \
-  --privileged -v /root:/host-rootfs motion-pro-vpn-client \
-  --method $METHOD --host $HOST --user $USER --passwd $PASSWD -c inf --loglevel warn
-
-# podman run -d --hostname motion-pro-vpn --name VPNcontainer --privileged --network host -v /root:/host-rootfs motion-pro-vpn-client --method $METHOD --host $HOST --user $USER --passwd $PASSWD -c inf --loglevel warn
-# podman run -d --hostname motion-pro-vpn --name VPNcontainer --privileged motion-pro-vpn-client --method $METHOD --host $HOST --user $USER --passwd $PASSWD -c inf --loglevel warn
