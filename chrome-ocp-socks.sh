b73c97f72cbf7302b6b4d8e4adc57fcd9a68ab35
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-11-07 20:41:45 +0800

    Update chrome-ocp-socks.sh

--- chrome-ocp-socks.sh
+++ chrome-ocp-socks.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
+set -euo pipefail
 
 # Basic Configuration
-VPN_MACHINE_IP="10.0.79.55"  
+VPN_MACHINE_IP="10.0.79.55"
 VPN_MACHINE_USER="root"
 PROXY_PORT="8899"
 
 # Usually no need to change
 CHROME_APP="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 TMP_PROFILE=$(mktemp -d)
-# URL="https://console-openshift-console.apps.ocp.example.com"
 
 # Set up SSH SOCKS5 proxy
-ssh -fN -D 127.0.0.1:${PROXY_PORT} ${VPN_MACHINE_USER}@${VPN_MACHINE_IP}
+if /usr/bin/pgrep -f "ssh -fN -D 127.0.0.1:${PROXY_PORT}" >/dev/null; then
+    echo "ok [SSH proxy port 127.0.0.1:${PROXY_PORT} already running]"
+else
+    if /usr/bin/ssh -fN -D 127.0.0.1:${PROXY_PORT} ${VPN_MACHINE_USER}@${VPN_MACHINE_IP}; then
+        echo "ok [SSH proxy started on 127.0.0.1:${PROXY_PORT} forwarding to ${VPN_MACHINE_IP}]"
+    else
+        echo "fail [SSH proxy started on 127.0.0.1:${PROXY_PORT} forwarding to ${VPN_MACHINE_IP}]"
+    fi
+fi
 
 # Launch Chrome (temporary profile + new window)
-"${CHROME_APP}" \
-  --proxy-server="socks5://127.0.0.1:${PROXY_PORT}" \
+echo "ok [Chrome launched with SOCKS5 proxy 127.0.0.1:${PROXY_PORT}]"
+"${CHROME_APP}" --proxy-server="socks5://127.0.0.1:${PROXY_PORT}" \
   --user-data-dir="${TMP_PROFILE}" \
-  --no-first-run --no-default-browser-check \
-#  --new-window "${URL}"
+  --disable-features=DarkMode,WebUIDarkMode \
+  --no-first-run --no-default-browser-check > /dev/null 2>&1
+# --new-window "${URL}" # URL="https://console-openshift-console.apps.ocp.example.com"
 
-# Clean up temporary profile after Chrome exits
-rm -rf "${TMP_PROFILE}"
+# Clean up temporary Chrome profile
+if rm -rf "${TMP_PROFILE}"; then
+    echo "ok [Clean up temporary Chrome profile]"
+else
+    echo "fail [Clean up temporary Chrome profile]"
+fi
