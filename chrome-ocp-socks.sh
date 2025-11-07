2acfdb534c9564223e60096ef7667509735abe72
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2025-11-08 02:47:10 +0800

    Update chrome-ocp-socks.sh

--- chrome-ocp-socks.sh
+++ chrome-ocp-socks.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# Run this script on a Mac to access the OpenShift Console
 set -euo pipefail
 
 # Basic Configuration
 VPN_MACHINE_IP="10.0.79.55"
 VPN_MACHINE_USER="root"
+INGRESS_DOMAIN=apps.ocp.example.com
 PROXY_PORT="8899"
 
 # Usually no need to change
 CHROME_APP="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-TMP_PROFILE=$(mktemp -d)
 
 # Set up SSH SOCKS5 proxy
 if /usr/bin/pgrep -f "ssh -fN -D 127.0.0.1:${PROXY_PORT}" >/dev/null; then
@@ -21,17 +22,49 @@ else
     fi
 fi
 
+# Create a Chrome bookmarks file in the temporary profile
+TMP_PROFILE=$(mktemp -d)
+mkdir -p "${TMP_PROFILE}/Default"
+if cat > "${TMP_PROFILE}/Default/Bookmarks" <<EOF
+{
+  "checksum": "dummy",
+  "roots": {
+    "bookmark_bar": {
+      "children": [
+        {
+          "type": "url",
+          "name": "vSphere Console",
+          "url": "https://vcenter.cee.ibmc.devcluster.openshift.com",
+          "date_added": "17625387360000000"
+        },
+        {
+          "type": "url",
+          "name": "OpenShift Console",
+          "url": "https://console-openshift-console.${INGRESS_DOMAIN}/dashboards",
+          "date_added": "17625390150000000"
+        }
+      ],
+      "name": "Bookmarks Bar",
+      "type": "folder"
+    },
+    "other": { "children": [], "name": "Other bookmarks", "type": "folder" },
+    "synced": { "children": [], "name": "Mobile bookmarks", "type": "folder" }
+  },
+  "version": 1
+}
+EOF
+then
+    echo "ok [Create a Chrome bookmarks file in the temporary profile]"
+else
+    echo "fail [Create a Chrome bookmarks file in the temporary profile]"
+fi
+
 # Launch Chrome (temporary profile + new window)
 echo "ok [Chrome launched with SOCKS5 proxy 127.0.0.1:${PROXY_PORT}]"
 "${CHROME_APP}" --proxy-server="socks5://127.0.0.1:${PROXY_PORT}" \
   --user-data-dir="${TMP_PROFILE}" \
   --disable-features=DarkMode,WebUIDarkMode \
   --no-first-run --no-default-browser-check > /dev/null 2>&1
-# --new-window "${URL}" # URL="https://console-openshift-console.apps.ocp.example.com"
 
 # Clean up temporary Chrome profile
-if rm -rf "${TMP_PROFILE}"; then
-    echo "ok [Clean up temporary Chrome profile]"
-else
-    echo "fail [Clean up temporary Chrome profile]"
-fi
+rm -rf "${TMP_PROFILE}"
