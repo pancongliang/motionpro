e5e9ede93ce60d02beb52bc8f9a5318b49acb219
Congliang Pan <75927592+pancongliang@users.noreply.github.com>
2026-03-09 08:02:20 +0800

    Update ssh-tunnel-chrome.sh

--- ssh-tunnel-chrome.sh
+++ ssh-tunnel-chrome.sh
@@ -32,7 +32,7 @@ TARGET_MACHINE_USER="root"
 
 # Define common SSH options for stability and persistence
 SSH_COMMON_OPTS=(
-    -N 
+    -C -fN 
     -D "127.0.0.1:${PROXY_PORT}" 
     -o ServerAliveInterval=15 
     -o ServerAliveCountMax=3 
